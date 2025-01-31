b47ec7e5de4e319481165cfc9adb48fb91f3381c
Sangeeta Santokee <santokee.sangeeta@gmail.com>
2025-01-31 14:11:20 -0400

    Lab updates

--- wsgi.py
+++ wsgi.py
@@ -12,3 +12,34 @@ def initialize():
   bob = User('bob', 'bob@mail.com', 'bobpass')
   print(bob)
   print('database intialized')
+  db.session.add(bob)
+  db.session.commit()
+  print(bob)
+  
+@app.cli.command("get-user", help="Retrieves a User")
+@click.argument('username', default='bob')
+def get_user(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print(f'{username} not found!')
+    return
+  print(bob)
+  
+@app.cli.command('get-users')
+def get_users():
+  # gets all objects of a model
+  users = User.query.all()
+  print(users)
+  
+@app.cli.command("change-email")
+@click.argument('username', default='bob')
+@click.argument('email', default='bob@mail.com')
+def change_email(username, email):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  bob.email = email
+  db.session.add(bob)
+  db.session.commit()
+  print(bob)
