8047c781143b13801da5516f195d0ebff3ea87e9
Sangeeta Santokee <157147731+SangeetaSantokee@users.noreply.github.com>
2025-02-07 17:44:16 +0000

    Assistant checkpoint: Move Todo model to models.py and clean up wsgi.py
    Assistant generated file changes:
    - models.py: Add Todo model
    - wsgi.py: Clean up wsgi.py
    
    ---
    
    User prompt:
    
    In the shell, I see the following:
    
    ```
    Error: While importing 'wsgi', an ImportError was raised:
    
    Traceback (most recent call last):
      File "/home/runner/workspace/.pythonlibs/lib/python3.10/site-packages/flask/cli.py", line 219, in locate_app
        __import__(module_name)
      File "/home/runner/workspace/wsgi.py", line 2, in <module>
        from models import db, User, Todo
    ImportError: cannot import name 'Todo' from 'models' (/home/runner/workspace/models.py)
    
    Usage: flask [OPTIONS] COMMAND [ARGS]...
    Try 'flask --help' for help.
    
    Error: No such command 'init'.
    ```
    
    If applicable, propose a fix immediately.

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,6 @@ from models import db, User, Todo
 from app import app
 from sqlalchemy.exc import IntegrityError
 
-
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
   db.drop_all()
@@ -15,7 +14,7 @@ def initialize():
   db.session.add(bob)
   db.session.commit()
   print(bob)
-  
+
 @app.cli.command("get-user", help="Retrieves a User")
 @click.argument('username', default='bob')
 def get_user(username):
@@ -24,13 +23,12 @@ def get_user(username):
     print(f'{username} not found!')
     return
   print(bob)
-  
+
 @app.cli.command('get-users')
 def get_users():
-  # gets all objects of a model
   users = User.query.all()
   print(users)
-  
+
 @app.cli.command("change-email")
 @click.argument('username', default='bob')
 @click.argument('email', default='bob@mail.com')
@@ -43,7 +41,7 @@ def change_email(username, email):
   db.session.add(bob)
   db.session.commit()
   print(bob)
-  
+
 @app.cli.command('create-user')
 @click.argument('username', default='rick')
 @click.argument('email', default='rick@mail.com')
@@ -54,39 +52,21 @@ def create_user(username, email, password):
     db.session.add(newuser)
     db.session.commit()
   except IntegrityError as e:
-    #let's the database undo any previous steps of a transaction
     db.session.rollback()
-    # print(e.orig) #optionally print the error raised by the database
-    print("Username or email already taken!") #give the user a useful message
+    print("Username or email already taken!")
   else:
-    print(newuser) # print the newly created user
+    print(newuser)
 
-  @app.cli.command('delete-user')
-  @click.argument('username', default='bob')
-  def delete_user(username):
-    bob = User.query.filter_by(username=username).first()
-    if not bob:
-        print(f'{username} not found!')
-        return
-    db.session.delete(bob)
-    db.session.commit()
-    print(f'{username} deleted')
+@app.cli.command('delete-user')
+@click.argument('username', default='bob')
+def delete_user(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  db.session.delete(bob)
+  db.session.commit()
+  print(f'{username} deleted')
 
-class Todo(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #set userid as a foreign key to user.id 
-  text = db.Column(db.String(255), nullable=False)
-  done = db.Column(db.Boolean, default=False)
-
-  def toggle(self):
-    self.done = not self.done
-    db.session.add(self)
-    db.session.commit()
-
-  def __init__(self, text):
-      self.text = text
-
-  def __repr__(self):
-
-    return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "done" if self.done else "not done" }>'
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
